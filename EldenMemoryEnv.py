fabc9b9049a934be6a6d62502188f3f542f5a0a1
joeru <102632048+1ceseismic@users.noreply.github.com>
2025-08-12 17:28:31 +1200

    move to openai gynamisum (not gym)

--- EldenMemoryEnv.py
+++ EldenMemoryEnv.py
@@ -1,7 +1,7 @@
-import gym
+import gymnasium as gym
 import time
 import numpy as np
-from gym import spaces
+from gymnasium import spaces
 
 from MemoryClient import MemoryClient
 from EldenRewardMemory import EldenRewardMemory
