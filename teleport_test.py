735d371511b1d64db44f18b49f570debc77c61c6
1ceseismic <102632048+1ceseismic@users.noreply.github.com>
2025-08-24 15:46:47 +1200

    use pymem.thread for remote threads

--- teleport_test.py
+++ teleport_test.py
@@ -1,5 +1,6 @@
 import pymem
 import pymem.process
+import pymem.thread # Import the thread module
 import time
 import struct
 
@@ -19,13 +20,13 @@ class MemoryManager:
     def free_memory(self, address): pymem.memory.free_memory(self.process_handle, address)
     def write_bytes(self, address, data): self.pm.write_bytes(address, data, len(data))
     def create_remote_thread(self, address):
-        # Correctly call create_remote_thread from pymem.process, passing the process handle
-        thread_handle = pymem.process.create_remote_thread(self.process_handle, address, 0)
+        # Correctly call create_remote_thread from pymem.thread
+        thread_handle = pymem.thread.create_remote_thread(self.process_handle, address, 0)
         if thread_handle:
-            # Correctly call wait_for_single_object from pymem.process, passing the thread handle
-            pymem.process.wait_for_single_object(thread_handle, -1) # -1 means wait indefinitely
-            # Correctly call close_handle from pymem.process, passing the thread handle
-            pymem.process.close_handle(thread_handle)
+            # Correctly call wait_for_single_object from pymem.thread
+            pymem.thread.wait_for_single_object(thread_handle, -1) # -1 means wait indefinitely
+            # Correctly call close_handle from pymem.thread
+            pymem.thread.close_handle(thread_handle)
             return True
         return False
 
